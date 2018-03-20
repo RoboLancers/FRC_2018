817197559c87d033d7dcea48c2dfc8ef2aeed7e2
Brian Chen <brianchen0810@gmail.com>
2018-03-20 17:49:49 -0400

    Add Comment to RobotUtil

--- src/org/usfirst/frc/team321/robot/utilities/RobotUtil.java
+++ src/org/usfirst/frc/team321/robot/utilities/RobotUtil.java
@@ -2,20 +2,48 @@ package org.usfirst.frc.team321.robot.utilities;
 
 public class RobotUtil {
 	
-	public static double range(double input, double min, double max){
-		if(input > max) {
-			return max;
-		} else if(input < min) {
-			return min;
-		} else {
-			return input;
-		}
+	private static final double RADIUS = 3.1;
+	private static final double CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+	private static final double TPR = 4096;
+	public static final double DISTANCE_PER_PULSE = CIRCUMFERENCE / TPR;
+	
+	/**
+	 * Returns the total encoder ticks to move a distance
+	 * @param targetDistance The distance in meters to move
+	 * @return Return amount of encoder ticks
+	 */
+	public static double getTargetTicks(double targetDistance){
+		return (targetDistance * 10 / DISTANCE_PER_PULSE); 
 	}
 	
-	public static double range(double value, double max){
-		return value > max ? max : (value < -max ? -max : value);
+	/**
+	 * Limits value to a specified minimum and maximum
+	 * @param value The value to check
+	 * @param min The lower bound
+	 * @param max The upper bound
+	 * @return Returns a number that is between min and max inclusive
+	 */
+	public static double range(double value, double min, double max){
+		return Math.min(max, Math.max(min, value));
 	}
-
+	
+	/**
+	 * Helper method for case where min and max are same number but one has a negative sign
+	 * @see RobotUtil#range(double, double, double)
+	 * @param value The value to limit
+	 * @param max The maximum value the number can take
+	 * @return Returns a number that is between -max and -max inclusive
+	 */
+	public static double range(double value, double max){
+		return range(value, -max, max);
+	}
+	
+	/**
+	 * Floors a number to a specific number of decimal places
+	 * @param num The number to floor
+	 * @param places How many decimal places to use
+	 * @return Returns the number floored
+	 */
 	public static double floor(double num, int places) {
 		double multiplier = 1;
 		
@@ -28,26 +56,31 @@ public class RobotUtil {
 		return (int)num / multiplier;
 	}
 	
-	public static double squareKeepSign(double num){	
-		if(num < 0){
-			return -(num * num);
-		}else{
-			return num * num;
-		}
+	/**
+	 * Keep the sign when squaring
+	 * @param num The number to square
+	 * @return The number squared including signs
+	 */
+	public static double squareKeepSign(double num){
+		return num < 0 ? -(num *num) : (num * num);
 	}
 	
+	/**
+	 * Keep the sign when square rooting
+	 * @param num The number to square root
+	 * @return The number square rooted including signs
+	 */
 	public static double sqrtKeepSign(double num) {
-		if(num < 0){
-			return -(Math.sqrt(-num));
-		}else{
-			return Math.sqrt(num);
-		}
+		return num < 0 ? -(Math.sqrt(-num)) : Math.sqrt(num);
 	}
 	
 	/**
 	 * Calculates the robot's motor speed to move to a target/angle
-	 * 
-	 * Note: Set power to 0 to move in place
+	 * @param power The power to move at. If it is 0 then it will turn in place
+	 * @param currentAngle Current robot angle
+	 * @param targetAngle Target angle to move to
+	 * @param kP How fast to get to target. The P term in PID
+	 * @return Returns the motor powers in an array
 	 */
 	public static double[] moveToTarget(double power, double currentAngle, double targetAngle, double kP) {
 		double[] motorSpeed = new double[2];
@@ -59,18 +92,5 @@ public class RobotUtil {
 	    motorSpeed[1] = RobotUtil.floor(motorSpeed[1], 2);
 	    
 	    return motorSpeed;
-	}
-	
-	public static double[] arcadeDrive(double moveValue, double rotateValue) {
-	    double leftMotorSpeed = range(-moveValue + rotateValue, -1, 1);
-	    double rightMotorSpeed = range(-moveValue - rotateValue, -1, 1);
-	    
-	    /*
-	    left = y_axis + x_axis
-		right = y_axis - x_axis
-	    */
-	    double[] values = {leftMotorSpeed, rightMotorSpeed};
-	    
-	    return values;
 	}
 }
